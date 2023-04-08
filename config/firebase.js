7d26ae8d666fd1871152e4eba563c9509042131a
Rıfat Kaşıkcı <rifatkasikci@gmail.com>
2023-04-09 00:24:22 +0300

    Added new ability for saving images to firebase storage service after get from OpenAI API...

---
+++ config/firebase.js
@@ -0,0 +1,18 @@
+const { initializeApp } = require("firebase/app");
+const { getStorage } = require('firebase/storage');
+require('dotenv').config()
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app, 'gs://ai-pitchdeck.appspot.com');
+
+module.exports = { app, storage };
--- scripts/helpers/openaiHelper.js
+++ scripts/helpers/openaiHelper.js
@@ -1,8 +1,11 @@
 const { Configuration, OpenAIApi } = require("openai")
 require('dotenv').config()
+const { storage } = require('../../config/firebase')
+const { ref, uploadBytes, getDownloadURL } = require("firebase/storage");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
+const axios = require('axios')
 
 const openai = new OpenAIApi(configuration)
 
@@ -11,10 +14,16 @@ const createCompletion = async (options) => {
     return response.data.choices[0].text
 }
 
-const generateImages = async(options) => {
-    const response = await openai.createImage(options)
-    return response.data.data[0].url
+const generateImages = async (options) => {
+  const randomId = Math.random().toString(36).substring(2);
+  const storageRef = ref(storage, `images/${randomId}.png`);
+  const response = (await openai.createImage(options)).data.data[0].url
+  const image = await axios.get(response, { responseType: 'arraybuffer' })
+  await uploadBytes(storageRef, image.data).then((snapshot) => {
+    console.log('Uploaded a raw string!');
+  })
+  return await getDownloadURL(storageRef)
 }
 
-module.exports = {createCompletion, generateImages, openai}
+module.exports = { createCompletion, generateImages, openai }
 
