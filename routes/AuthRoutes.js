cd4c1fd8ae65750be227fc5b1c7198bed656ad62
Rıfat Kaşıkcı <rifatkasikci@gmail.com>
2023-07-09 20:39:48 +0300

    Added validation middleware and validation rules

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const AuthController = require('../contollers/AuthController')
 const RefreshTokenController = require('../contollers/RefreshTokenController')
 const UserTokenController = require('../contollers/UserTokenController')
+const userValidationRules = require('../validationRules/userValidationRules')
+const ValidationMiddleware = require('../middlewares/validationMiddleware').validate
 
 router.route('/login').post(AuthController.login)
-router.route('/register').post(AuthController.register)
+router.route('/register').post(ValidationMiddleware(userValidationRules), AuthController.register)
 router.route('/change-password/:token').post(UserTokenController.verifyUserTokenAndChangePassword)
 router.route('/reset-token').post(RefreshTokenController.resetToken)
 router.route('/reset-password').post(UserTokenController.createResetPasswordToken)
--- routes/PitchDeckRoutes.js
+++ routes/PitchDeckRoutes.js
@@ -1,11 +1,13 @@
 const router = require('express').Router()
 const PitchDeckController = require('../contollers/PitchDeckController')
 const AuthMiddleware = require('../middlewares/AuthMiddleware').requireAuth
+const pitchDeckValidationRules = require('../validationRules/pitchDeckValidationRules')
+const ValidationMiddleware = require('../middlewares/validationMiddleware').validate
 
 router.route('/').get(AuthMiddleware('Admin'), PitchDeckController.getAll)
 router.route('/getByUserId').get(AuthMiddleware('Admin,User'),PitchDeckController.getByUserId)
 router.route('/:id').get(AuthMiddleware('Admin,User'),PitchDeckController.getById)
-router.route('/add').post(AuthMiddleware('Admin,User'),PitchDeckController.generatePitchDeck)
+router.route('/add').post(ValidationMiddleware(pitchDeckValidationRules), AuthMiddleware('Admin,User'),PitchDeckController.generatePitchDeck)
 router.route('/chat').post(AuthMiddleware('Admin,User'),PitchDeckController.getChat)
 
 module.exports = router
