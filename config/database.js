5ce17942ac8600850d5f1c118d214cc4542086c9
Rıfat Kaşıkcı <rifatkasikci@gmail.com>
2023-07-11 16:41:21 +0300

    Updated some configurations

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 module.exports = {
     "development": {
-        "storage":"./db/db-test.sqlite",
+        "storage":"./db/db-test.sqlite3",
         "dialect": "sqlite"
     },
     "production": {
-        "storage":"./db/db.sqlite",
+        "storage":"./db/db.sqlite3",
         "dialect": "sqlite"
     }
   }
