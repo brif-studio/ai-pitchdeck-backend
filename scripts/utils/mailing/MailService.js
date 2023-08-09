ee85150f70043a66831a63211405c518f3a3769a
Umut Karapınar <umut.karapinar01@gmail.com>
2023-08-09 19:20:46 +0300

    son ayarlar yapılyor......

--- scripts/utils/mailing/MailService.js
+++ scripts/utils/mailing/MailService.js
@@ -6,8 +6,10 @@ class MailService {
     constructor(mailService) {
         this.mailService = mailService
     }
+  //  await MailService.sendMail(new Mail(email, 'password reset test', emailTemplate(passwordResetLink)))
 
     sendMail(mail) {
+
         rabitmqHelper.publishToExchange('sys.mail', JSON.stringify(mail));
         // return this.mailService.sendMail(mail)
     }
--- scripts/utils/mailing/run.js
+++ scripts/utils/mailing/run.js
@@ -3,15 +3,17 @@ const Mail = require("./Mail")
 const emailTemplate = require('./templates/emailTemplate').emailTemplate
 const rabitmqHelper = require("../../helpers/rabbitmqHelper")
 
-let mail = {
-    "from": '"Denem mail" <support@linyit.net>',
-    "to": "mail.to",
-    "subject": "mail.subject",
-    "html": "mail.html"
-};
+
 async function asd() {
-    for (let i = 0; i < 10001; i++) {
-        await rabitmqHelper.publishToExchange('sys.mail', JSON.stringify(mail))
+    for (let i = 0; i < 1; i++) {
+        // let mail = {
+        //     "from": 'support@linyit.net',
+        //     "to": "dtv76833@zbock.com",
+        //     "subject": i.toString(),
+        //     "html": emailTemplate("passwordResetLink").toString()
+        // };
+
+        MailService.sendMail(new Mail("umut.karapinar01@gmail.com", i.toString(), emailTemplate("passwordResetLink")));
 
 
         console.log(i)
@@ -20,4 +22,3 @@ async function asd() {
 }
 
 asd()
-
